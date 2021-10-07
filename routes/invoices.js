51e6bea36451565892613eec145fe1b8e9a84c61
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2021-10-07 08:30:44 -0700

    Update invoices.js

---
+++ routes/invoices.js
@@ -0,0 +1,68 @@
+//sql db queries are run asyncisly
+//routes for /invoices
+const express = require("express");
+const ExpressError = require('../expressError');
+const router = new express.Router();
+const db = require('../db')
+
+/** returns info for all invoices */
+router.get('', async (req, res, next) => {    
+    try {
+        const results = await db.query('SELECT * FROM invoices') 
+        return res.send(results.rows); //maybe change to{invoices: {}}
+    } catch (error) {
+        return next(new ExpressError("Couldn't find data",404));
+    }
+});
+
+router.post('', async (req, res, next) => {    
+    const req.body.code;
+    const req.body.name;
+    const req.body.description;
+    try {
+        const results = await db.query('SELECT * FROM companies') 
+        return res.send(results.rows); //maybe change to{companies: {}}
+    } catch (error) {
+        return next(new ExpressError("Couldn't add company",404));
+    }
+});
+
+router.get('/:code', async (req, res, next) => {    
+    try {
+        const results = await db.query('SELECT * FROM companies WHERE code=code', //add sanitizer) 
+        return res.send({company: {results}}); //might need to check what is returned
+    } catch (error) {
+        return next(new ExpressError("Couldn't find company",404));
+    }
+});
+
+router.put('/:code', async (req, res, next) => {    
+    const code = req.params.code;
+    const name = req.body.name;
+    const description = req.body.description;
+    
+    try {
+        const results = await db.query('SELECT * FROM companies WHERE code=code', //add sanitizer) 
+        //reassign results stuff
+        return res.send({company: {results}}); //might need to check what is returned
+    } catch (error) {
+        return next(new ExpressError("Couldn't find company",404));
+    }
+});
+
+router.delete('/:code', async (req, res, next) => {    
+    const code = req.params.code;
+    const name = req.body.name;
+    const description = req.body.description;
+    
+    try {
+        const results = await db.query('SELECT * FROM companies WHERE code=code', //add sanitizer) 
+        //then del it
+        return res.send({company: {results}}); //might need to check what is returned
+    } catch (error) {
+        return next(new ExpressError("Couldn't find company",404));
+    }
+});
+
+
+module.exports = router;
