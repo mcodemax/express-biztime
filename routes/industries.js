6481301d4e46f439f78053a71f290716a7292afe
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2021-10-12 08:27:41 -0700

    Update industries.js

--- routes/industries.js
+++ routes/industries.js
@@ -4,6 +4,7 @@ const express = require("express");
 const ExpressError = require('../expressError');
 const router = new express.Router();
 const db = require('../db')
+const slugify = require('slugify');
 
 //add routes for:
 // adding an industry
@@ -13,58 +14,50 @@ const db = require('../db')
 /** returns info for all industries */
 router.get('', async (req, res, next) => {    
     try {
-        const results = await db.query(`SELECT invoices.id, invoices.comp_code FROM invoices`); // using * in SQL is bad practice
+        const results = await db.query(`SELECT code, name, description FROM industries`); 
         return res.send({invoices: results.rows}); 
     } catch (error) {
         return next(new ExpressError("Couldn't find data", 404));
     }
 });
 
-/** return obj on given invoice*/
-router.get('/:id', async (req, res, next) => {    
+/** return obj on given added industry*/
+router.post('', async (req, res, next) => {    
     try {
-        const id = req.params.id;
+        const { name, description } = req.body;
         
-        //might have to do a LEFT JOIN to display all of invoices.id, invoices.amt, invoices.comp_code, invoices.paid, invoices.add_date, invoices.paid_date
-        const results = await db.query(`SELECT invoices.id, invoices.amt, invoices.comp_code, invoices.paid, invoices.add_date, invoices.paid_date 
-                                        FROM invoices
-                                        JOIN companies ON invoices.comp_code=companies.code
-                                        WHERE invoices.id=$1`
-                                      ,[id]); 
+        //slugify the indsutry
+        const code = slugify(name, { lower: true, strict: true });
         
-        if(!results.rows[0]) throw new ExpressError("Couldn't find invoice", 404);
+        const results = await db.query(
+            `INSERT INTO industries (code, name, description)
+            VALUES ($1, $2, $3)
+            RETURNING code, name, description`,
+            [code, name, description]
+        );
 
-        const comp_code = results.rows[0].comp_code;
-        if(results.rows[0].hasOwnProperty('comp_code')) delete results.rows[0]['comp_code'];
-        
-        const comp_results = await db.query(`SELECT code, name, description FROM companies
-                                            WHERE code=$1`
-                                            ,[comp_code]);
-
-        results.rows[0].company = comp_results.rows[0];
-
-        return res.send( { invoice: results.rows[0]} );  //might need to check what is returned
+        return res.send( { industry: results.rows[0]} );  //might need to check what is returned
     } catch (error) {
-        return next(error);
+        return next(new ExpressError('Could not add industry';, 404));
     }
 });
 
-/** add an invoice
+/** add an industry to a company (association an industry with a company)
  * I: {"comp_code":"apple", "amt":20000}
 */
-router.post('', async (req, res, next) => {       
+router.post('/associations', async (req, res, next) => {       
     try {
-        const {comp_code, amt} = req.body;
+        const {comp_code, indus_code} = req.body;
         
         const results = await db.query(
-            `INSERT INTO invoices (comp_code, amt)
+            `INSERT INTO companies_industries (comp_code, indus_code)
             VALUES ($1, $2)
-            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
-            [comp_code, amt]
+            RETURNING comp_code, indus_code`,
+            [comp_code, indus_code]
         );
-        return res.status(201).send( {invoice: results.rows[0] } ); //maybe change to{invoices: {}}
+        return res.status(201).send( { "indsutry association": results.rows[0] } ); //maybe change to{invoices: {}}
     } catch (error) {
-        return next(new ExpressError("Couldn't add invoice",404));
+        return next(new ExpressError("Couldn't add association", 404));
     }
 });
 
